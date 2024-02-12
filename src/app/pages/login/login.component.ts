a5b5d9f58ef8dbe6f7fb89f60135353b1cc19af9
Edwin Fom <edwinfom05@gmail.com>
2024-02-12 11:29:08 +0100

    modified

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  formData = {
+    email: '',
+    password: '',
+    google_id: '',
+  };
 
+  showSuccessAlert = false;
+  constructor(private httpClient: HttpClient) {}
+
+  Login() {
+    this.httpClient
+      .post('http://localhost:8000/api/login', this.formData)
+      .subscribe((readData: any) => {
+        console.log(readData);
+        this.showSuccessAlert = true; // Affiche l'alerte
+        this.formData.email = '';
+        this.formData.password = '';
+      });
+  }
+  LoginWithGoogle() {
+    this.httpClient
+      .post('http://localhost:8000/api/login/google', this.formData)
+      .subscribe((readData: any) => {
+        console.log(readData);
+
+        this.formData.google_id = '';
+      });
+  }
 }
