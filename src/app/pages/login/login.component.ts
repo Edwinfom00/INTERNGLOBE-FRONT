bc15e94f62895e6ac3319350ee3d4debf70bb74e
Edwin Fom <edwinfom05@gmail.com>
2024-02-25 01:37:17 +0100

    modified

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -19,11 +19,17 @@ export class LoginComponent {
   Login() {
     this.httpClient
       .post('http://localhost:8000/api/login', this.formData)
-      .subscribe((readData: any) => {
-        console.log(readData);
-        this.showSuccessAlert = true; // Affiche l'alerte
-        this.formData.email = '';
-        this.formData.password = '';
+      .subscribe((response: any) => {
+        if (response.access_token) {
+          // Stocker le jeton d'accès dans le stockage local
+          localStorage.setItem('access_token', response.access_token);
+          // Vous pouvez également stocker d'autres informations utilisateur si nécessaire
+          // localStorage.setItem('user', JSON.stringify(response.user));
+
+          this.showSuccessAlert = true; // Affiche l'alerte
+          this.formData.email = '';
+          this.formData.password = '';
+        }
       });
   }
   LoginWithGoogle() {
@@ -34,5 +40,10 @@ export class LoginComponent {
 
         this.formData.google_id = '';
       });
+  }
+
+  // Fonction pour récupérer le jeton d'accès stocké
+  getAccessToken(): string | null {
+    return localStorage.getItem('access_token');
   }
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,8 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  constructor(private httpclient: HttpClient) {}
+
+  getHeaders(): HttpHeaders {
+    const accessToken = localStorage.getItem('access_token');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    });
+  }
+
   FormData = {
     name: '',
+    email: '',
     address: '',
     gender: '',
     dob: '',
@@ -17,4 +29,26 @@ export class ProfileComponent {
     resume: '',
     avatar: '',
   };
+
+  UpdateProfile() {
+    this.httpclient
+      .put('http://localhost:8000/api/user/profile/update', this.FormData, {
+        headers: this.getHeaders(),
+      })
+      .subscribe((resultData: any) => {
+        console.log(resultData);
+
+        this.FormData.name = '';
+        this.FormData.address = '';
+        this.FormData.gender = '';
+        this.FormData.name = '';
+        this.FormData.dob = '';
+        this.FormData.phone = '';
+        this.FormData.bio = '';
+        this.FormData.cover_letter = '';
+        this.FormData.resume = '';
+        this.FormData.name = '';
+        this.FormData.avatar = '';
+      });
+  }
 }
